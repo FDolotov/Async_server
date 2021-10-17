6817c827ec526e14c4ba7298bd5d27957ff51946
SirJorah <59660051+FDolotov@users.noreply.github.com>
2021-10-17 23:38:17 +0300

    Add files via upload

---
+++ utils.hpp
@@ -0,0 +1,18 @@
+#include <string>
+
+namespace utils
+{
+	template <class T> T to_int(const std::string& s)
+	{
+  		T base{10};
+  		T result{0};
+  		
+		for (auto c : s)
+		{
+    			result = base * result + static_cast<T>(c - '0');
+  		};
+  		
+		return result;
+	};
+
+};
